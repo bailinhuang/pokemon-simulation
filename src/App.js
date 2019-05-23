fe045814a87385baf4cc080785411fd3820d07f7
Bailin Huang <bailinhuangq@hotmail.com>
2019-05-23 08:35:19 -0600

    project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board/Board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Board/>
     </div>
   );
 }
---
+++ src/components/Board/Board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Board extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default Board
