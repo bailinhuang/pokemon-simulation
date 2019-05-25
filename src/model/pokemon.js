e95b418b4d82ec09a32b53f2876fe9658409c100
Bailin Huang <bailinhuangq@hotmail.com>
2019-05-24 21:16:51 -0600

    added models

---
+++ src/model/pokemon.js
@@ -0,0 +1,9 @@
+class Pokemon {
+  constructor(x, y, hp, attackRange, attackDamage) {
+    this.x = x;
+    this.y = y;
+    this.hp = hp;
+    this.attackRange = attackRange;
+    this.attackDamage = attackDamage;
+  } 
+}
